a90f8973119eb27566cd6de2fade416980c97ba6
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-07-24 03:05:12 +0200

    2024-07-24 01:48 : id 134 -> /nix/store/z3bsg7899aig8amlckqzyflhpxh3gg10-home-manager-generation

--- home.nix
+++ home.nix
@@ -19,6 +19,12 @@
     };
   };
 
+  hyprland.monitors = [
+    "Ancor Communications Inc ASUS PB277 0x00008283, 2560x1440@74.92, 0x0, 1"
+    "ASUSTek COMPUTER INC VG259 L8LMQS009978, 1920x1080@143.61, auto-right, 1"
+    "Unknown-1,disable"
+  ];
+
   fonts.fontconfig.enable = true;
 
   programs.home-manager.enable = true;
