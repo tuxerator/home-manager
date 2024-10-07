73ff75613a4fbc10d58226c36d4068f6b34ea96e
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-10-08 01:51:57 +0200

    add rofi and obs

---
+++ desktop/rofi/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, lib, ... }:
+
+{
+  programs.rofi = {
+    enable = true;
+    plugins = with pkgs; [
+      rofi-rbw
+    ];
+    extraConfig = {
+      modi = "run, window, ssh, combi";
+    };
+  };
+}
---
+++ programs/obs.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+{
+  programs.obs-studio.enable = true;
+  programs.obs-studio.plugins = [
+    pkgs.obs-studio-plugins.droidcam-obs
+  ];
+}
