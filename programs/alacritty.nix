1acc48efe0eab9f435d583bdd8a35b103931bea6
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-10-29 10:57:59 +0100

    2024-10-29 10:57 : id 16 -> /nix/store/lb14nlifq7v4kpcgs4cya3m3wv0gm97d-home-manager-generation

--- programs/alacritty.nix
+++ programs/alacritty.nix
@@ -3,8 +3,16 @@
   programs.alacritty = {
     enable = true;
     settings = {
+      colors = {
+        primary = {
+          background = "#1d1f21";
+          bright_foreground = "#eaeaea";
+          dim_foreground = "#828482";
+          foreground = "#c5c8c6";
+        };
+      };
       window = {
-        opacity = 0.0;
+        opacity = 0.7;
       };
     };
   };
