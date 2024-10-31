2a18c2a9b19c352e364a06bc6056b082121a6d05
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-10-31 11:11:39 +0100

    2024-10-31 11:11 : id 32 -> /nix/store/64fbid6jvspzgvx3k3fi4y6jzzk0b9ds-home-manager-generation

--- programs/neovim/lazy/lua/plugins/nabla.lua
+++ programs/neovim/lazy/lua/plugins/nabla.lua
@@ -2,6 +2,6 @@ return {
   "https://github.com/jbyuki/nabla.nvim.git",
   config = function()
     require("nabla").enable_virt()
-    vim.keymap.set("n", "<lder>p", require("nabla").popup)
+    vim.keymap.set("n", "<leader>p", require("nabla").popup)
   end,
 }
