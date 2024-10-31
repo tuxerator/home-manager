092d83d074096bb6fcf473267cf9bc479adf0430
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-10-31 14:55:14 +0100

    add stylix

---
+++ desktop/stylix.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  stylix = {
+    polarity = "dark";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+    };
+    fonts = {
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "Meslo" "Hack" ]; };
+        name = "Hack Nerd Font";
+      };
+
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+    };
+  };
+}
