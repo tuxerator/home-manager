e92002c3e7efe8d51cea87b7a203916646755a39
Jakob Sanowski <jakob.sanowski@sanbach.de>
2024-12-02 14:13:36 +0100

    2024-12-02 14:13 : id 96 -> /nix/store/mfip7qdp2dxl08a97ybgqmb26lpiwpvx-home-manager-generation

--- programs/neovim/lazy/lua/plugins/tiny-inline-diagnostic.lua
+++ programs/neovim/lazy/lua/plugins/tiny-inline-diagnostic.lua
@@ -2,5 +2,7 @@ return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "VeryLazy", -- Or `LspAttach`
   priority = 1000, -- needs to be loaded in first
-  opts = {},
+  opts = {
+    preset = "amongus",
+  },
 }
