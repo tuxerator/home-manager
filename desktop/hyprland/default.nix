72e9ca3ef8ed1b434f9ddb0f4e95e5b1caa9ce61
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-01-08 12:47:32 +0100

    2025-01-08 12:47 : id 133 -> /nix/store/3bh9b2s92j657874y27kwdwv19qxpchm-home-manager-generation

--- desktop/hyprland/default.nix
+++ desktop/hyprland/default.nix
@@ -211,11 +211,12 @@ with lib;
           ", switch:on:Lid Switch, exec, ${pkgs.writeShellScript "on-lid-close" ''
           eww close bar
           hyprctl keyword monitor "eDP-1, disable"
+          sleep 0.5
           eww open bar''}"
           ", switch:off:Lid Switch, exec, ${pkgs.writeShellScript "on-lid-close" ''
           eww close bar
           hyprctl keyword monitor "eDP-1, preferred, 0x0, 1"
-          sleep 0.2
+          sleep 0.5
           eww open bar''}"
         ];
       };
