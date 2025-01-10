c3dafb760aab734b227cc5c9d7ed3c8bda821a10
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-01-10 19:26:54 +0100

    2025-01-10 19:26 : id 159 -> /nix/store/rmf7aq7kcj2aqik85plc9fsxqgp11zqw-home-manager-generation

--- desktop/hyprland/hypridle.nix
+++ desktop/hyprland/hypridle.nix
@@ -1,41 +1,28 @@
 { pkgs, ... }: {
   options = { };
-  config = let
-  in {
+  config = {
     services.hypridle = {
       enable = true;
       settings = {
-        general = { ignore_dbus_inhibit = false; };
+        general = {
+          before_sleep_cmd = "loginctl lock-session;";
+          after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
+          ignore_dbus_inhibit = false;
+          lock_cmd =
+            "${pkgs.sysvtools}/bin/pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
+        };
 
         listener = [
           {
-            timeout = 30;
+            timeout = 600;
             on-timeout = "loginctl lock-session";
           }
           {
-            timeout = 40;
+            timeout = 700;
             on-timeout = "systemctl suspend-then-hibernate";
             on-resume = "eww close bar; eww open bar";
           }
         ];
-      };
-    };
-
-    systemd.user.services = {
-      hyprlock = {
-        Unit = {
-          Description = "Lock screen daemon";
-          OnSucess = "unlock.target";
-          PartOf = "lock.target";
-          After = "lock.target";
-        };
-
-        Install = { WantedBy = [ "lock.target" ]; };
-        Service = {
-          ExecStart = "${pkgs.hyprlock}/bin/hyprlock";
-          Type = "forking";
-          Restart = "on-failure";
-        };
       };
     };
   };
