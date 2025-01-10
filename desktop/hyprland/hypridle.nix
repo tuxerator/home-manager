b11dc357273290e658c6df089b003e3bbe72df2c
Jakob Sanowski <jakob.sanowski@sanbach.de>
2025-01-10 19:48:42 +0100

    2025-01-10 19:46 : id 161 -> /nix/store/iqw2gfd9m060xwrk0lhfvb65mn2waawz-home-manager-generation

--- desktop/hyprland/hypridle.nix
+++ desktop/hyprland/hypridle.nix
@@ -5,7 +5,8 @@
       enable = true;
       settings = {
         general = {
-          before_sleep_cmd = "loginctl lock-session";
+          before_sleep_cmd =
+            "${pkgs.sysvtools}/bin/pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock";
           after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
           ignore_dbus_inhibit = false;
           lock_cmd =
